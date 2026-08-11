c6c6d8a1966db280d98a098a1ebd4627f0cd7364
supasreymark-dev <supasreymark@gmail.com>
2026-08-11 21:43:45 +0800

    Updating main.py and requirements.txt

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,15 +1,17 @@
 import os
+import resend
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-import smtplib
-from email.message import EmailMessage
+# import smtplib
+# from email.message import EmailMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
-sender_email = os.getenv("GMAIL_ADDRESS")
-app_password = os.getenv("GMAIL_APP_PASSWORD")
+# sender_email = os.getenv("GMAIL_ADDRESS")
+# app_password = os.getenv("GMAIL_APP_PASSWORD")
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 app = FastAPI()
 
@@ -33,19 +35,39 @@ def send_email(email: emailRequest):
 
     receiver_email = "supasreymark@gmail.com"
 
-    msg = EmailMessage()
+    params = {
+        "from": "supasreymark@gmail.com",
+        "to": [receiver_email],
+        "subject": "Message coming from portfolio website",
+        "reply-to": email.email,
+        "html": f"""
+        <h2>New message from your portfolio</h2>
 
-    msg["Subject"] = "Message coming from portfolio website"
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
-    msg["Reply-To"] = email.email
+        <p><strong>Name:</strong>{email.name}</p>
 
-    msg.set_content(email.message)
+        <p><strong>Email:</strong>{email.email}</p>
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(sender_email, app_password)
-        server.send_message(msg)
+        <p><strong>Message:</strong></p>
+
+        <p>{email.message}</p>
+        """
+    }
+
+    response = resend.Emails.send(params)
+
+    # msg = EmailMessage()
+
+    # msg["Subject"] = "Message coming from portfolio website"
+    # msg["From"] = sender_email
+    # msg["To"] = receiver_email
+    # msg["Reply-To"] = email.email
+
+    # msg.set_content(email.message)
+
+    # with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    #     server.starttls()
+    #     server.login(sender_email, app_password)
+    #     server.send_message(msg)
 
     return{
         "success" : True,
